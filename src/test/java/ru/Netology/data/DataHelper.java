b882211d46e29b14974af4b44ffcefa6ca52fc62
vugar350 <Vugar350@mail.ru>
2022-03-26 08:28:13 +0300

    ошибка

---
+++ src/test/java/ru/Netology/data/DataHelper.java
@@ -0,0 +1,44 @@
+package ru.Netology.data;
+
+import lombok.Value;
+
+public class DataHelper {
+    private DataHelper() {
+    }
+
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    public static AuthInfo getOtherAuthInfo(AuthInfo original) {
+        return new AuthInfo("petya", "123qwerty");
+    }
+
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+        return new VerificationCode("12345");
+    }
+
+    @Value
+    public static class CardInfo {
+        private String id;
+        private String balance;
+    }
+
+    public static CardInfo getFirstCardInfo(CardInfo cardInfo) {
+        return new CardInfo("5559 0000 0000 0001", "10 000");
+    }
+    public static CardInfo getSecondCardInfo(CardInfo cardInfo) {
+        return new CardInfo("5559 0000 0000 0002", "10 000");
+    }
+}
---
+++ src/test/java/ru/Netology/page/DashboardPage.java
@@ -0,0 +1,30 @@
+package ru.Netology.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import lombok.val;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+
+    private ElementsCollection cards = $$(".list__item");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+
+    public void Dashboard() {
+    }
+
+    public int getCardBalance(String id) {
+        val text = cards.findBy(text());
+        return extractBalance(text);
+    }
+
+    private int extractBalance(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+}
